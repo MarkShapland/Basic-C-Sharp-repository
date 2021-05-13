78f7177c636cb7ad904babd8fd8eb2d7c4f7da1b
Mark Shapland <mark_shapland@hotmail.com>
2021-05-12 19:51:33 -0700

    console application

---
+++ Basic_C#_Programs/ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to have it multiplied by 50.");
+            string product = Console.ReadLine();
+            double Product = Convert.ToDouble(product);
+            double product50 = Product * 50;
+            Console.WriteLine(Product + " x 50 = " + product50);
+           
+
+            Console.WriteLine("Enter a number to have 25 added to it.");
+            string num1 = Console.ReadLine();
+            int num2 = Convert.ToInt32(num1);
+            int num3 = num2 + 25;
+            Console.WriteLine(num2 + " + 25 = " + num3);
+           
+
+            Console.WriteLine("Enter a number to have it divided by 12.5");
+            string num4 = Console.ReadLine();
+            double num5 = Convert.ToDouble(num4);
+            double num6 = num5 / 12.5;
+            Console.WriteLine(num5 + "/" + "25 = " + num6);
+            
+
+            Console.WriteLine("Enter a number to see if it is greater than 50.");
+            string num7 = Console.ReadLine();
+            int num8 = Convert.ToInt32(num7);
+            bool greaterOrLessthan = num8 > 50;
+            Console.WriteLine(greaterOrLessthan.ToString());
+            
+            Console.WriteLine("Enter a to see the remainder when it is devided by 7.");
+            string num9 = Console.ReadLine();
+            int num10 = Convert.ToInt32(num9);
+            double remainder = num10 % 7;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/Math_Challenge/Math_Challenge/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1 = 5;
+            //int num2 = 25;
+            //int total = num1 + num2;
+            //Console.WriteLine(total);
+
+
+            //int num3 = 25;
+            //int num4 = 5;
+            //int total2 = num3 - num4;
+            //Console.WriteLine(total2);
+
+            //int num5 = 25;
+            //int num6 = 25;
+            //int total3 = num5 * num6;
+            //Console.WriteLine(total3);
+
+
+            //int num7 = 100;
+            //int num8 = 10;
+            //int quotient = num7 / num8;
+            //Console.WriteLine(quotient);
+
+            //string firstName = "Mark ";
+            //int lastName = 12;
+            //Console.WriteLine(firstName + lastName);
+
+            // int total = 5 + 10;
+            // int othertotal = 12 + 22;
+            //int combined = total + othertotal;
+            // Console.WriteLine(combined);
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("ten minus five =  " + difference.ToString());
+            //Console.Read();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //double quotient = 200 / 3;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+            //modulus
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.Write(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 70;
+
+            //bool isWarm = currentTemperature == roomTemperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+
+
+
+        }
+    }
+}
